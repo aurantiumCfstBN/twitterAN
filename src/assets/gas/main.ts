a9f64c91aaf73c5402aa8ac002e9ab2c2788fc5b
aurantiumCfstBN <navyblue.sobvfs2438@gmail.com>
2019-01-06 22:44:33 +0900

    ""

--- src/assets/gas/main.ts
+++ src/assets/gas/main.ts
@@ -1,3 +1,10 @@
-function test(){
+function test() :void{
     //test!!!
+}
+
+function doGet() :any{
+    return HtmlService.createTemplateFromFile('html_index')
+        .evaluate()
+        .setSandboxMode(HtmlService.SandboxMode.IFRAME)
+        .setTitle("test");
 }
