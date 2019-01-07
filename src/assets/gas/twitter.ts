4a46f99b1e749b914ef9949548af51c50016f6e7
aurantiumCfstBN <navyblue.sobvfs2438@gmail.com>
2019-01-07 15:54:05 +0900

    ""

--- src/assets/gas/twitter.ts
+++ src/assets/gas/twitter.ts
@@ -1,5 +1,5 @@
-var exports = exports || {};
-var module = module || { exports: exports };
+"1CXDCY5sqT9ph64fFwSzVtXnbjpSfWdRymafDrtIZ7Z_hwysTY7IIhi7s";
+
 var key = handlePropertiesServiceGet([
     'TWITTER_CONSUMER_KEY',
     'TWITTER_CONSUMER_SECRET',
@@ -88,11 +88,44 @@ function postUpdateStatus() {
         payload: { status: "test" }
     });
 }
+
+function searchRegularly(){
+  var start = new Date();
+  const sleepTime = 30;  //sec
+  var result = [];
+
+  for(var i=0; i<15; i++){
+    result = result.concat(search());
+    Utilities.sleep(sleepTime * 1000);
+  }
+  result = result.filter((v,i,s) => {
+    return s.findIndex(_v => {
+      return v.id === _v.id;
+    }) === i;
+  });
+
+  var d = new Date();
+  var fileName = [
+    "searchPs_",
+    d.getFullYear(),
+    ("00" + (d.getMonth() + 1)).slice(-2),
+    ("00" + d.getDate()).slice(-2),
+    "_",
+    ("00" + d.getHours()).slice(-2),
+    ("00" + d.getMinutes()).slice(-2),
+    ("00" + d.getSeconds()).slice(-2),
+    "_",
+    ("0000" + d.getMilliseconds()).slice(-4),
+    ".json"
+  ].join("");
+  saveToDrive(outputFolder, fileName, result, {"JSON_visible":true});
+}
+
 // ツイートを検索
 function search() {
     var service = twitter.getService();
     //var response = service.fetch('https://api.twitter.com/1.1/search/tweets.json?q=%22%E3%83%96%E3%83%AB%E3%83%8A%E3%83%9D%22%20OR%20%22%E3%83%96%E3%83%AB%E3%83%BC%E3%83%8A%E3%83%9D%E3%83%AC%E3%82%AA%E3%83%B3%22%20OR%20%40hoge%20exclude%3Aretweets&count=100', { method: 'get' });
-    var response = service.fetch('https://api.twitter.com/1.1/search/tweets.json?q=%E3%83%95%E3%82%A9%E3%83%AD%E3%83%BC%E3%81%97%E3%81%A6%E3%80%81%E3%81%93%E3%81%AE%E3%83%84%E3%82%A4%E3%83%BC%E3%83%88%E3%82%92RT%E3%81%97%E3%81%9F%E3%82%89%E8%BF%94%E4%BF%A1%E3%81%8C%E5%B1%8A%E3%81%8D%E3%81%BE%E3%81%99%E3%82%88%EF%BC%81%E6%9C%9F%E9%96%93%E4%B8%AD%E6%AF%8E%E6%97%A5%E6%8A%95%E7%A8%BF%E3%81%99%E3%82%8B%E3%81%AE%E3%81%A7%E3%80%81%E6%AC%A0%E3%81%8B%E3%81%95%E3%81%9A%E3%83%81%E3%83%A3%E3%83%AC%E3%83%B3%E3%82%B8%E3%81%97%E3%81%BE%E3%81%97%E3%82%87%E3%81%86%EF%BC%81%E2%80%BB%E6%B7%B7%E9%9B%91%E3%81%AB%E3%82%88%E3%82%8A%E8%BF%94%E4%BF%A1%E3%81%AB%E3%81%8A%E6%99%82%E9%96%93%E3%81%8B%E3%81%8B%E3%82%8B%E5%A0%B4%E5%90%88%E3%81%8C%E3%81%82%E3%82%8A%E3%81%BE%E3%81%99retweets&count=100', { method: 'get' });
+    var response = service.fetch('https://api.twitter.com/1.1/search/tweets.json?q=%E3%83%95%E3%82%A9%E3%83%AD%E3%83%BC%E3%81%97%E3%81%A6%E3%80%81%E3%81%93%E3%81%AE%E3%83%84%E3%82%A4%E3%83%BC%E3%83%88%E3%82%92RT%E3%81%97%E3%81%9F%E3%82%89%E8%BF%94%E4%BF%A1%E3%81%8C%E5%B1%8A%E3%81%8D%E3%81%BE%E3%81%99%E3%82%88%EF%BC%81%E6%9C%9F%E9%96%93%E4%B8%AD%E6%AF%8E%E6%97%A5%E6%8A%95%E7%A8%BF%E3%81%99%E3%82%8B%E3%81%AE%E3%81%A7%E3%80%81%E6%AC%A0%E3%81%8B%E3%81%95%E3%81%9A%E3%83%81%E3%83%A3%E3%83%AC%E3%83%B3%E3%82%B8%E3%81%97%E3%81%BE%E3%81%97%E3%82%87%E3%81%86%EF%BC%81%E2%80%BB%E6%B7%B7%E9%9B%91%E3%81%AB%E3%82%88%E3%82%8A%E8%BF%94%E4%BF%A1%E3%81%AB%E3%81%8A%E6%99%82%E9%96%93%E3%81%8B%E3%81%8B%E3%82%8B%E5%A0%B4%E5%90%88%E3%81%8C%E3%81%82%E3%82%8A%E3%81%BE%E3%81%99&count=100', { method: 'get' });
     /*
     var response = service.fetch('https://api.twitter.com/1.1/search/tweets.json?', {
       method: 'get',
@@ -105,8 +138,8 @@ function search() {
 
     var createdDateOfData = (new Date()).toJSON();
 
-    var data = response.statuses.map((v,i,s) => {
-      var dataPiece;
+    var data = JSON.parse(response.getContentText()).statuses.map((v,i,s) => {
+      var dataPiece = {};
       [
         "id",
         "name",
@@ -124,26 +157,17 @@ function search() {
       return dataPiece;
     });
 
-    var d = new Date();
-    var fileName = [
-      "searchPs_",
-      d.getFullYear(),
-      ("00" + (d.getMonth() + 1)).slice(-2),
-      ("00" + d.getDate()).slice(-2),
-      "_",
-      ("00" + d.getHours()).slice(-2),
-      ("00" + d.getMinutes()).slice(-2),
-      ("00" + d.getSeconds()).slice(-2),
-      "_",
-      ("0000" + d.getMilliseconds()).slice(-4),
-      ".json"
-    ].join("");
-    outputFolder.createFile(
-      fileName,
-      JSON.stringify(
-        JSON.parse(response.getContentText()),
-        null,
-        "  "
-      )
-    );
+    return data;
+}
+
+function saveToDrive(folder, fileName, content, option){
+  option = (option === null || option === undefined ? {} : option);
+  folder.createFile(
+    fileName,
+    (
+      option.JSON_visible === true ?
+      JSON.stringify(content, null, "  ") :
+      JSON.stringify(content)
+    )
+  );
 }
